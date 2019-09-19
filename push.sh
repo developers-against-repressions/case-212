a55a112b69dc9f12590bbe0eb2e5760c3651bc34
Kim Gustyr <kgustyr@aori.ru>
2019-09-19 19:15:54 +0300

    Commit and push signature

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b temp
+  git add README.md
+  git commit --message "Add a signature. Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+git_push() {
+  git remote add origin https://${GH_TOKEN}@github.com/developers-against-repressions/case-212.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin temp
+}
+
+setup_git
+git_commit_readme
+git_push
