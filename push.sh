0e6f0bc00fb0c4c27e5269abd042bf085154523f
Kim Gustyr <kgustyr@aori.ru>
2019-09-19 19:31:36 +0300

    Also commit new signature file

--- push.sh
+++ push.sh
@@ -8,6 +8,7 @@ setup_git() {
 git_commit_readme() {
   git checkout -b temp
   git add README.md
+  git add signed/
   git commit --message "Add a signature. Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
